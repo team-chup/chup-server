791e6fe4ba051d9d70f8018b1caa3198156464ff
신희성 <vvwv3092@gmail.com>
2025-06-05 22:22:36 +0900

    update: signup service

--- src/main/java/gsm/gsmjava/domain/user/service/SingUpService.java
+++ src/main/java/gsm/gsmjava/domain/user/service/SingUpService.java
@@ -26,6 +26,7 @@ public class SingUpService {
         userRepository.save(currentUser);
 
         Resume resume = Resume.builder()
+                .user(currentUser)
                 .name(reqDto.getResume().getName())
                 .type(reqDto.getResume().getType())
                 .url(reqDto.getResume().getUrl())

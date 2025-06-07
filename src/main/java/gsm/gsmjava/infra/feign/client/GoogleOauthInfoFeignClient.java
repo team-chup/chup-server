181644f9b737565510586b7e3be09f4f0c1fa97b
신희성 <vvwv3092@gmail.com>
2025-06-07 23:35:36 +0900

    add: feign config

--- src/main/java/gsm/gsmjava/infra/feign/client/GoogleOauthInfoFeignClient.java
+++ src/main/java/gsm/gsmjava/infra/feign/client/GoogleOauthInfoFeignClient.java
@@ -1,15 +1,20 @@
 package gsm.gsmjava.infra.feign.client;
 
+import gsm.gsmjava.infra.feign.config.GoogleFeignConfig;
 import gsm.gsmjava.infra.feign.dto.GoogleInfoResDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "GoogleOauthInfoClient", url = "https://www.googleapis.com")
+@FeignClient(
+        name = "GoogleOauthInfoClient",
+        url = "https://www.googleapis.com",
+        configuration = GoogleFeignConfig.class
+)
 public interface GoogleOauthInfoFeignClient {
     @GetMapping(
-            value = "/oauth2/v1/userinfo",
-            consumes = "application/json"
+        value = "/oauth2/v1/userinfo",
+        consumes = "application/json"
     )
     GoogleInfoResDto getInfo(
         @RequestHeader("Authorization") String accessToken
---
+++ src/main/java/gsm/gsmjava/infra/feign/config/GoogleFeignConfig.java
@@ -0,0 +1,21 @@
+package gsm.gsmjava.infra.feign.config;
+
+import feign.Request;
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class GoogleFeignConfig {
+    @Bean
+    public Request.Options requestOptions() {
+        return new Request.Options(3000, 3000);
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100, TimeUnit.SECONDS.toMillis(1), 2);
+    }
+}

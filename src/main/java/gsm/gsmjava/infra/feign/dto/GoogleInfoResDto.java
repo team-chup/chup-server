0c52d307a857f3b32bd3cf30cf7a68653a194eb0
신희성 <vvwv3092@gmail.com>
2025-06-07 22:26:44 +0900

    delete: test code

--- src/main/java/gsm/gsmjava/infra/feign/dto/GoogleInfoResDto.java
+++ src/main/java/gsm/gsmjava/infra/feign/dto/GoogleInfoResDto.java
@@ -1,12 +1,10 @@
 package gsm.gsmjava.infra.feign.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class GoogleInfoResDto {
     private String email;
 }

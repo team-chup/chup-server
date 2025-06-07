bb51e86e6674c8450fcbcc35d637342ad286dfd7
신희성 <vvwv3092@gmail.com>
2025-06-07 23:05:43 +0900

    update GoogleInfoResDto constructor

--- src/main/java/gsm/gsmjava/infra/feign/dto/GoogleInfoResDto.java
+++ src/main/java/gsm/gsmjava/infra/feign/dto/GoogleInfoResDto.java
@@ -2,11 +2,16 @@ package gsm.gsmjava.infra.feign.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GoogleInfoResDto {
     private String email;
+
+    public GoogleInfoResDto(String email) {
+        this.email = email;
+    }
+
+    public GoogleInfoResDto() {
+    }
 }

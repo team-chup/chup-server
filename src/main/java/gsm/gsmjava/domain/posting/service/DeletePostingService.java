59332be0831cbd605a28adfd48002733ef56822a
신희성 <vvwv3092@gmail.com>
2025-06-12 14:05:01 +0900

    update: delete posting api cache evict

--- src/main/java/gsm/gsmjava/domain/posting/service/DeletePostingService.java
+++ src/main/java/gsm/gsmjava/domain/posting/service/DeletePostingService.java
@@ -4,9 +4,12 @@ import gsm.gsmjava.domain.posting.entity.Posting;
 import gsm.gsmjava.domain.posting.repository.PostingRepository;
 import gsm.gsmjava.global.error.ExpectedException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static gsm.gsmjava.global.cache.CacheConstant.POSTING_LIST_CACHE;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class DeletePostingService {
     private final PostingRepository postingRepository;
 
     @Transactional
+    @CacheEvict(value = POSTING_LIST_CACHE, key = "'ALL'", cacheManager = "cacheManager")
     public void delete(Long postingId) {
         Posting posting = postingRepository.findById(postingId).orElseThrow(
                 () -> new ExpectedException("해당 공고는 존재하지 않습니다. id = " + postingId, HttpStatus.NOT_FOUND)
